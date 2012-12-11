98f11c30ef718e03f75d2d5ae67b4892b511ae27
Brad McMullen <mcmullenbrad@hotmail.com>
2012-12-11 00:48:56 -0500

    Timer Speed, Start/Stop/Reset Button Functionality, TextView Updating

--- Project 2 - Utility App - Brad McMullen/src/ca/georgian/project2/MainActivity.java
+++ Project 2 - Utility App - Brad McMullen/src/ca/georgian/project2/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.georgian.project2;
 
+import java.text.DecimalFormat;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,13 +20,21 @@ public class MainActivity extends Activity {
 	Button lapbutton;
 	Button resetbutton;
 	double seconds = 0;
-    double minutes = 0;
-    double hours = 0;
+	//Used to get fastest laps and slowest laps if I implement it.
+	double totalloops = 0;
+	double totalloopshigh = 0;
+	//Arbitrary starting point so every lap will be lower than this.
+	double totalloopslow = 1000000000;
+	////////////////////////////////////////////////////////////////
+    Integer minutes = 0;
+    Integer hours = 0;
     String timeStr = "";
     private Handler mHandler = new Handler();
     TextView timeDisplay;
     long mStartTime = 0L;
     String secondsstr;
+    String minutestr;
+    String hourstr;
     
     @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -38,10 +47,22 @@ public class MainActivity extends Activity {
 		lapbutton = (Button) findViewById(R.id.lapbtn);
 		resetbutton = (Button) findViewById(R.id.resetbtn);
 	
-		//Buttons
+		//Stop Button
 		stopbutton.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
 				mHandler.removeCallbacks(mUpdateTimeTask);
+				mStartTime = 0L;
+				startbutton.setClickable(true);
+				resetbutton.setClickable(true);
+			}
+		});
+		
+		resetbutton.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				timeDisplay.setText("0:00:00.0");
+				seconds = 0;
+				minutes = 0;
+				hours = 0;
 			}
 		});
 		
@@ -53,6 +74,8 @@ public class MainActivity extends Activity {
 		            mStartTime = System.currentTimeMillis();
 		            //mHandler.removeCallbacks(mUpdateTimeTask);
 		            mHandler.postDelayed(mUpdateTimeTask, 100);
+		            startbutton.setClickable(false);
+		            resetbutton.setClickable(false);
 		       }
 			}
 				
@@ -64,15 +87,58 @@ public class MainActivity extends Activity {
     private Runnable mUpdateTimeTask = new Runnable() {
 		   public void run() {
 			   seconds = seconds + 0.1;
-			   secondsstr = Double.toString(seconds);
-			   secondsstr = String.format("%.1g%", secondsstr);
-				if(seconds > 9.9){
+			   totalloops++;
+			   //Decimal Formatting, to tenths of a second
+			   DecimalFormat onedeci = new DecimalFormat("#0.0");
+			   secondsstr = onedeci.format(seconds);
+			   minutestr = Integer.toString(minutes);
+			   hourstr = Integer.toString(hours);
+			   
+			   
+			   if(seconds > 59.9){
+				   seconds = 0;
+				   minutes++;
+			   }
+			   
+			   if(minutes > 59){
+				   minutes = 0;
+				   hours++;
+			   }
+			   	//Label Updating
+			   	//Seconds Only
+				if(seconds > 9.9 && minutes == 0){
 					timeDisplay.setText("0:00:" + secondsstr);
 				}
-				else{
+				else if (seconds <= 9.9 && minutes == 0){
 					timeDisplay.setText("0:00:0" + secondsstr);
 				}
-				mHandler.postDelayed(mUpdateTimeTask, 100);
+				//Seconds and minutes
+				else if (seconds <= 9.9 && minutes >= 0 && minutes <= 10){
+					timeDisplay.setText("0:0" + minutestr + ":" + "0" + secondsstr);
+				}
+				else if (seconds > 9.9 && minutes >= 0 && minutes <= 10){
+					timeDisplay.setText("0:0" + minutestr + ":" + secondsstr);
+				}
+				else if (seconds <= 9.9 && minutes > 9){
+					timeDisplay.setText("0:" + minutestr + ":" + "0" + secondsstr);
+				}
+				else if (seconds > 9.9 && minutes > 9){
+					timeDisplay.setText("0:" + minutestr + ":" + secondsstr);
+				}
+				//Seconds, Minutes and Hours
+				else if (seconds <= 9.9 && minutes >= 0 && minutes <= 9 && hours <= 9){
+					timeDisplay.setText(hours + ":" + "0" + minutes + ":" + "0" + seconds);
+				}
+				else if (seconds > 9.9 && minutes >= 0 && minutes <= 9 && hours <= 9){
+					timeDisplay.setText(hours + ":" + "0" + minutes + ":" + seconds);
+				}
+				else if (seconds <= 9.9 && minutes > 9 && hours <= 9){
+					timeDisplay.setText(hours + ":" + minutes + ":" + "0" + seconds);
+				}
+				else{
+					timeDisplay.setText(hours + ":" + minutes + ":" + seconds);
+				}
+				mHandler.postDelayed(mUpdateTimeTask, 50);
 		   }
 		};//
 		
